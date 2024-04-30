d037fe5ae9d6071a949ad086f5994d8fd9af8589
강우승 <kung0615@gnu.ac.kr>
2024-04-30 15:22:15 +0900

    add the run file

---
+++ run_recbole_copy.py
@@ -0,0 +1,39 @@
+# @Time   : 2020/7/20
+# @Author : Shanlei Mu
+# @Email  : slmu@ruc.edu.cn
+
+# UPDATE
+# @Time   : 2020/10/3, 2020/10/1
+# @Author : Yupeng Hou, Zihan Lin
+# @Email  : houyupeng@ruc.edu.cn, zhlin@ruc.edu.cn
+
+import argparse
+import time
+from recbole.quick_start import run_recbole
+
+
+if __name__ == '__main__':
+    begin = time.time()
+    parameter_dict = {
+        'neg_sampling': None
+        # 'multi_gpus': [0,1,2,3]
+        # 'gpu_id':3,
+        # 'attribute_predictor':'not',
+        # 'attribute_hidden_size':"[256]",
+        # 'fusion_type':'gate',
+        # 'seed':212,
+        # 'n_layers':4,
+        # 'n_heads':1
+    }
+    
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--model', '-m', type=str, default='IRIS', help='name of models')
+    parser.add_argument('--dataset', '-d', type=str, default='Amazon_Musical_Instruments', help='name of datasets')
+    parser.add_argument('--config_files', type=str, default='configs/Amazon_Musical_Instruments.yaml', help='config files')
+
+    args, _ = parser.parse_known_args()
+
+    config_file_list = args.config_files.strip().split(' ') if args.config_files else None
+    run_recbole(model=args.model, dataset=args.dataset, config_file_list=config_file_list,config_dict=parameter_dict)
+    end=time.time()
+    print(end-begin)
